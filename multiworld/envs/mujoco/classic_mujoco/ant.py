275cd924c35dafce15a213082ecdedc1eb01ac09
Coline Devin <coline@berkeley.edu>
2019-07-02 10:26:57 -0700

    better goal sampling

--- multiworld/envs/mujoco/classic_mujoco/ant.py
+++ multiworld/envs/mujoco/classic_mujoco/ant.py
@@ -5,12 +5,14 @@ from multiworld.core.multitask_env import MultitaskEnv
 from multiworld.core.serializable import Serializable
 from multiworld.envs.env_util import get_stat_in_paths, create_stats_ordered_dict, get_asset_full_path
 from multiworld.envs.mujoco.mujoco_env import MujocoEnv
+from pyquaternion import Quaternion
+
 try:
     import torch
 except ImportError:
     pass
 
-class HalfCheetahEnv(MujocoEnv, MultitaskEnv, Serializable):
+class AntEnv(MujocoEnv, MultitaskEnv, Serializable):
     def __init__(self, action_scale=1, frame_skip=5, reward_type='vel_distance', indicator_threshold=0.1, fixed_goal=5, fix_goal=False, max_speed=6, 
                  use_vel_in_goal=False, use_vel_in_state=True, history_len = 3,
                 ):
@@ -33,10 +35,11 @@ class HalfCheetahEnv(MujocoEnv, MultitaskEnv, Serializable):
         self.history  = []
         obs_size = self._get_obs()['observation'].shape[0]
 
-        high = np.array([2,0.5,1.05, 0.785, .785, .7,  .87, .5,np.pi])
-        low =  np.array([-2,-0.5,-0.52, -0.785, -0.4, -1, -1.2, -.5 ,np.pi])
+        # Angles are in degrees
+        high = np.array([ 1, 1, 2,1, 180, 180, 360, 30,70, 30,-30, 30,-30, 30,70])
+        low =  np.array([-1,-1,0.5,-1, -180,-180,-360,-30,30,-30,-70,-30,-70,-30,30])
         
-        high_vel = np.zeros(9)
+        high_vel = np.zeros(high.shape[0])
         high_tot = np.concatenate((high, high_vel))
         low_tot = np.concatenate((low, -1*high_vel))
         if self.use_vel_in_goal:
@@ -60,16 +63,12 @@ class HalfCheetahEnv(MujocoEnv, MultitaskEnv, Serializable):
             ('state_desired_goal', self.goal_space),
             ('state_achieved_goal', self.achieved_goal_space),
         ])
-        if self.use_vel_in_goal:
-            self.goals = np.load('/home/coline/hexiang/cheetah_states_with_vel.npy')
-        else:
-            self.goals = np.load('/home/coline/hexiang/cheetah_states.npy')
         self.reset()
         self.imsize = 500
 
     @property
     def model_name(self):
-        return get_asset_full_path('classic_mujoco/half_cheetah.xml')
+        return get_asset_full_path('classic_mujoco/ant.xml')
 
     def step(self, action):
         action = action * self.action_scale
@@ -85,14 +84,15 @@ class HalfCheetahEnv(MujocoEnv, MultitaskEnv, Serializable):
             obs = np.concatenate([self.sim.data.qpos.flat, self.sim.data.qvel.flat])
         else:
             obs =  np.concatenate([self.sim.data.qpos.flat])
+            #print(self.sim.data.qpos.flat[3:6], np.linalg.norm(self.sim.data.qpos.flat[3:6]))
         return obs
 
     def _get_obs(self, add_to_history=False):
         state_obs = self._get_env_obs()
         hist_obs = self._get_obs_with_history(state_obs, add_to_history=add_to_history)
-        achieved_goal = state_obs[:9]
-        if self.use_vel_in_goal:
-            achieved_goal = state_obs
+        achieved_goal = state_obs
+        if self.use_vel_in_state and not self.use_vel_in_goal:
+            achieved_goal = state_obs[:int(state_obs.shape[0]/2)]
         return dict(
             observation=hist_obs,
             desired_goal=self._state_goal,
@@ -116,9 +116,9 @@ class HalfCheetahEnv(MujocoEnv, MultitaskEnv, Serializable):
 
     def _get_info(self, ):
         state_obs = self._get_env_obs()
-        xpos = state_obs[:9]
-        if self.use_vel_in_goal:
-            xpos = state_obs
+        xpos = state_obs
+        if self.use_vel_in_state and not self.use_vel_in_goal:
+            xpos = state_obs[:int(state_obs.shape[0]/2)]
         desired_xpos = self._state_goal
         xpos_error = np.linalg.norm(xpos - desired_xpos)
         info = dict()
@@ -127,14 +127,23 @@ class HalfCheetahEnv(MujocoEnv, MultitaskEnv, Serializable):
         info['state_success'] = (xpos_error < self.indicator_threshold).astype(float)
         info['per_joint_success'] =np.mean((np.abs(xpos - desired_xpos) < self.indicator_threshold).astype(float))
         return info
+    
+    def weight_difference(self,difference):
+        if len(difference.shape) == 1:
+            difference[6:15] = difference[6:15]/180.
+        else:
+            difference[:,6:15] = difference[:,6:15]/180.
+        return difference
 
     def compute_rewards(self, actions, obs):
         achieved_goals = obs['achieved_goal']
         desired_goals = obs['desired_goal']
+        difference = achieved_goals - desired_goals
+        difference = self.weight_difference(difference)
         if isinstance(achieved_goals,  np.ndarray):
-            distances = np.linalg.norm(achieved_goals - desired_goals, axis=1)
+            distances = np.linalg.norm(difference, axis=1)
         else:
-            distances = torch.norm(achieved_goals - desired_goals, dim=1)
+            distances = torch.norm(difference, dim=1)
         if self.reward_type == 'vel_distance':
             r = -distances
         elif self.reward_type == 'vel_success':
@@ -209,6 +218,12 @@ class HalfCheetahEnv(MujocoEnv, MultitaskEnv, Serializable):
                 self.goal_space.high,
                 size=(batch_size, self.goal_space.low.size),
             )
+            # Ant has a free joint in indices 0:6, and indices 3:6 are a quaternion.
+            # Instead of sampling those value independently, we sample from a 4D unit
+            # Gaussian, divide by first value so that it is 1, and pass the last 3 values
+            angles = np.random.normal(loc=0.0, scale=1.0, size=(batch_size, 4))
+            angles = angles/np.linalg.norm(angles, axis=1)
+            goals[:, 3:6] = angles[:,1:]
 #             goals_idx = np.random.choice(self.goals.shape[0], size=(batch_size))
 #             goals = self.goals[goals_idx]
         return {
@@ -231,7 +246,7 @@ class HalfCheetahEnv(MujocoEnv, MultitaskEnv, Serializable):
         self._state_goal = goal
 
 if __name__ == "__main__":
-    env = HalfCheetah()
+    env = Ant()
     env.get_goal()
     env.step(np.array(1))
     env.reset()
